b832a6be786891880b066b1f0d17ccfabf09cb71
miautawn <miautawn@gmail.com>
2026-07-26 12:53:51 +0200

    Add stuff

---
+++ dot_zen/miautawn_profile/user.js
@@ -0,0 +1,47 @@
+// IDK, just guessing which settings need to be static
+user_pref("browser.aboutConfig.showWarning", false);
+user_pref("browser.bookmarks.addedImportButton", true);
+user_pref("browser.bookmarks.restore_default_bookmarks", false);
+
+user_pref("browser.search.region", "US");
+
+user_pref("browser.translations.automaticallyPopup", false);
+user_pref("browser.translations.mostRecentTargetLanguages", "en");
+user_pref("browser.urlbar.placeholderName", "Google");
+
+user_pref("doh-rollout.doneFirstRun", true);
+user_pref("doh-rollout.home-region", "US");
+
+user_pref("dom.forms.autocomplete.formautofill", true);
+user_pref("extensions.formautofill.addresses.enabled", false);
+user_pref("extensions.formautofill.creditCards.enabled", false);
+
+user_pref("extensions.pictureinpicture.enable_picture_in_picture_overrides", true);
+
+user_pref("extensions.ui.dictionary.hidden", true);
+user_pref("extensions.ui.extension.hidden", false);
+user_pref("extensions.ui.locale.hidden", true);
+user_pref("extensions.ui.mlmodel.hidden", true);
+user_pref("extensions.ui.sitepermission.hidden", true);
+
+user_pref("findbar.highlightAll", true);
+
+user_pref("sidebar.visibility", "hide-sidebar");
+user_pref("signon.rememberSignons", false);
+
+user_pref("zen.glance.activation-method", "ctrl");
+user_pref("zen.mediacontrols.enabled", false);
+user_pref("zen.pinned-tab-manager.restore-pinned-tabs-to-pinned-url", true);
+user_pref("zen.tabs.show-newtab-vertical", false);
+user_pref("zen.ui.migration.compact-mode-button-added", true);
+
+user_pref("zen.view.compact.enable-at-startup", false);
+user_pref("zen.view.compact.toolbar-flash-popup", true);
+user_pref("zen.view.show-newtab-button-top", false);
+user_pref("zen.view.use-single-toolbar", false);
+user_pref("zen.welcome-screen.seen", true);
+user_pref("privacy.exposeContentTitleInWindow", false);
+user_pref("zen.workspaces.continue-where-left-off", true);
+
+// force dark mode
+user_pref("layout.css.prefers-color-scheme.content-override", 0);
